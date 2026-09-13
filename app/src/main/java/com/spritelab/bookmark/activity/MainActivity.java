90c1511fd2ec63c4c2b046eb5469c2d2212040bd
Sprite Asus <net53648@gmail.com>
2026-09-13 12:18:53 +0500

    Down tabs have been added

--- app/src/main/java/com/spritelab/bookmark/activity/MainActivity.java
+++ app/src/main/java/com/spritelab/bookmark/activity/MainActivity.java
@@ -10,6 +10,9 @@ import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
+import androidx.cardview.widget.CardView;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
 
 import com.google.gson.Gson;
 import com.kongzue.dialogx.DialogX;
@@ -29,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
     ImageView btnSettings;
     EditText etBookMark;
     ImageView btnSend;
+    CardView cardNotes, cardTasks;
 
     private static final String TAG = "class:MainActivity";
     private boolean cooldown;
@@ -41,10 +45,14 @@ public class MainActivity extends AppCompatActivity {
         btnSettings = findViewById(R.id.btnSettings);
         etBookMark = findViewById(R.id.etBookMark);
         btnSend = findViewById(R.id.btnSend);
+        cardNotes = findViewById(R.id.card_notes);
+        cardTasks = findViewById(R.id.card_tasks);
 
         loadConfig();
         initDialogX();
         listeners();
+        updateTabs(true);
+        replaceFragment(new NotesFragment());
     }
 
     private void listeners() {
@@ -81,6 +89,29 @@ public class MainActivity extends AppCompatActivity {
                 etBookMark.setText("");
             }
         }, () -> {});
+
+        HelpUtils.setupDropAnimation(cardNotes, false, () -> {
+            updateTabs(true);
+            replaceFragment(new NotesFragment());
+        }, () -> {});
+
+        HelpUtils.setupDropAnimation(cardTasks, false, () -> {
+            updateTabs(false);
+            // Если в будущем появится TasksFragment, здесь можно будет вызывать: replaceFragment(new TasksFragment());
+        }, () -> {});
+    }
+
+    private void updateTabs(boolean isNotesActive) {
+        int activeColor = ContextCompat.getColor(this, R.color.aluminum);
+        int transparentColor = ContextCompat.getColor(this, R.color.transparent);
+
+        if (isNotesActive) {
+            cardNotes.setCardBackgroundColor(activeColor);
+            cardTasks.setCardBackgroundColor(transparentColor);
+        } else {
+            cardNotes.setCardBackgroundColor(transparentColor);
+            cardTasks.setCardBackgroundColor(activeColor);
+        }
     }
 
     private void loadConfig() {
@@ -120,5 +151,11 @@ public class MainActivity extends AppCompatActivity {
         DialogX.globalTheme = DialogX.THEME.DARK;
         DialogX.globalStyle = new IOSStyle();
         DialogX.backgroundColor = Color.parseColor("#222222");
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment_container_view, fragment)
+                .commit();
     }
 }
